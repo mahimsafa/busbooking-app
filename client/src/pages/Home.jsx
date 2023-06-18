c3666bf98db3ff36c3750a5bdea0002c0927f165
Mahim Safa <hi@mahimsafa.com>
2023-06-18 13:22:41 +0600

    first commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import busStopImage from "../assets/busstop.png";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/esm/Button";
+
+
+function Home() {
+  return (
+    <Container>
+      <Row className="mt-4">
+        <Col className="col-12 col-lg-6 d-flex flex-column justify-content-center">
+          <h1>Bus Booking System</h1>
+          <h4>Book tickets hoe.</h4>
+          <Link to="/booking">
+            <Button
+              variant="primary"
+              type="submit"
+            >
+              Book Now
+            </Button>
+          </Link>
+        </Col>
+        <Col className="col-12 col-lg-6">
+          <img src={busStopImage} alt="Bus Stop" className="w-100"></img>
+        </Col>
+        
+      </Row>
+    </Container>
+  );
+}
+
+export default Home;
---
+++ client/src/utils/constants.js
@@ -0,0 +1,31 @@
+export const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+export const cities = [
+  "New York",
+  "Pakistan",
+  "India",
+  "Rochester",
+  "Delhi",
+  "baby",
+,
+];
+
+export const filters = [
+  "Pretty",
+  "South",
+  "Baby",
+  "Non-AC",
+  "Wi-Fi",
+  "Charger",
+  "Movies",
+];
+
+export const sorters = ["Price"];
